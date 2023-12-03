e14b59a71198c805ef470ea8e88878527d70508e
weasleysx <ptmwilfk@gmail.com>
2023-12-03 16:51:20 +0700

    dont'use

--- firebase/index.tsx
+++ firebase/index.tsx
@@ -1,14 +1,14 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import firebaseClient from "./clientApp";
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// import firebaseClient from "./clientApp";
 
-// Other imports if you're using other Firebase services
+// // Other imports if you're using other Firebase services
 
-const firebaseConfig = {
-  // Your Firebase configuration object
-};
+// const firebaseConfig = {
+//   // Your Firebase configuration object
+// };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
 
-export { db };
+// export { db };
