00d80f47667d903680164986b3ab7880a70ea4c7
weasleysx <ptmwilfk@gmail.com>
2023-12-03 17:02:57 +0700

    delete unuse

--- components/splash/Splash.tsx
+++ components/splash/Splash.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
-import hero from "../../images/hero.png";
+
 
 export default function Splash() {
   return (
